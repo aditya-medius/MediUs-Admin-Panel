25df65a243520e9b7648a985cbd8d9b934215094
hema <hemakumari1745@gmail.com>
2022-04-02 20:50:13 +0530

    UIfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'argon-dashboard-angular';
+
+  fileToUpload: File | null = null;
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  postFile(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+
+      let fileName = file.name;
+
+      const formData = new FormData();
+
+      formData.append("file", file);
+      console.log(formData)
+      this.httpClient.post("http://3.21.52.154:3000/admin/uploadCSV_locality", {
+       formData
+      })
+        .subscribe((result: any) => {
+          console.log(result)
+        })
+
+
+
+    }
+
+  }
 }
