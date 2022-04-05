96cdf382c153a2d4863d8ff690e82d2a1191573b
hema <hemakumari1745@gmail.com>
2022-04-05 10:30:29 +0530

    UIfix

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { LocationService } from "src/app/services/location.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: "app-user-profile",
@@ -7,8 +8,12 @@ import { LocationService } from "src/app/services/location.service";
   styleUrls: ["./user-profile.component.scss"],
 })
 export class UserProfileComponent implements OnInit {
-  constructor(private locationService?: LocationService) {}
-
+  title='toaster-not';
+  constructor(private locationService?: LocationService, private toastr?:ToastrService) {}
+  showToasterSuccess(){
+    this.toastr.success("Data shown successfully !!", "Data shown successfully !!")
+  }
+  
   sff_data: Array<any> | null = null;
   ngOnInit() {
     this.sff_data = [
@@ -24,6 +29,27 @@ export class UserProfileComponent implements OnInit {
     const formData: FormData = new FormData();
     formData.append("file", fileToUpload, fileToUpload.name);
     this.locationService.uploadCSV_state(formData).subscribe((result: any) => {
+      if (result.status === 200) {
+      }
+    });
+  }
+
+  postCity(fileToUpload: File) {
+    fileToUpload = fileToUpload[0];
+    const formData: FormData = new FormData();
+    formData.append("file", fileToUpload, fileToUpload.name);
+    this.locationService.uploadCSV_city(formData).subscribe((result: any) => {
+      if (result.status === 200) {
+      }
+    });
+  }
+
+  postLocality(fileToUpload: File) 
+  {
+    fileToUpload = fileToUpload[0];
+    const formData: FormData = new FormData();
+    formData.append("file", fileToUpload, fileToUpload.name);
+    this.locationService.uploadCSV_locality(formData).subscribe((result: any) => {
       if (result.status === 200) {
       }
     });
