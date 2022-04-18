592a3a23384a52db4064d4d6809846d3b5135c7d
aditya@thedreamsteps.com <aditya@thedreamsteps.com>
2022-04-18 16:40:06 +0530

    new changes

--- src/app/pages/specializations/specializations.component.ts
+++ src/app/pages/specializations/specializations.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { SpecializationsService } from "src/app/services/specializations.service";
 import { UserProfileComponent } from "../user-profile/user-profile.component";
-import { HttpService } from 'src/app/services/http.service';
+import { HttpService } from "src/app/services/http.service";
 import { ToastrService } from "ngx-toastr";
 
 export interface PeriodicElement {
@@ -10,18 +10,6 @@ export interface PeriodicElement {
   name: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Hydrogen' },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Helium', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Lithium', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Beryllium', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Boron', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Carbon', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Nitrogen' },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Oxygen', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Fluorine', },
-  { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png", name: 'Neon', },
-];
 @Component({
   selector: "app-specializations",
   templateUrl: "./specializations.component.html",
@@ -29,16 +17,28 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class SpecializationsComponent
   extends UserProfileComponent
-  implements OnInit {
+  implements OnInit
+{
   arr = [];
-   data =[];
-   textInput = '';
-   displayValue: string;
+  data = [];
+  textInput = "";
+  displayValue: string;
   posts: any;
-  constructor(private specializationService: SpecializationsService,
-    private httpService: HttpService, protected toastr?: ToastrService) {
+  constructor(
+    private specializationService: SpecializationsService,
+    private httpService: HttpService,
+    protected toastr?: ToastrService
+  ) {
     super();
   }
+
+  ELEMENT_DATA: PeriodicElement[] = [
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+      name: "Hydrogen",
+    },
+  ];
 
   showToasterSuccess() {
     this.toastr.success(
@@ -48,9 +48,8 @@ export class SpecializationsComponent
   }
 
   ngOnInit(): void {
-
     this.sff_data = [
-      { title: "Speciality", cb: this.specializationService.addSpeciality },
+      // { title: "Speciality", cb: this.specializationService.addSpeciality },
       { title: "Body Part", cb: this.specializationService.addBodyPart },
       { title: "Disease", cb: this.specializationService.addDisease },
       { title: "Doctor Type", cb: this.specializationService.addDoctorType },
@@ -61,38 +60,41 @@ export class SpecializationsComponent
       },
     ];
 
-      this.httpService.getcomments().subscribe(
-        (result : any) => {
-          this.posts =result;
-          this.data=result
-          console.log(result);
-
-          this.httpService.getPosts().subscribe(
-            (response:any) => {
-              this.posts = response;
-              this.arr=response
-                console.log(response);
-            },
-            (error:any) => { console.log(error); });
-        } )
+    this.httpService
+      .getListOfSpecialityBodyPartAndDisease()
+      .subscribe((result: any) => {
+        this.ELEMENT_DATA = result.data.Speciality.map((e: any) => {
+          return {
+            name: e.specialityName,
+            image: e.specialityName,
+          };
+        });
+        this.dataSource = this.ELEMENT_DATA;
+      });
   }
-  onClick(event){
-    console.log(this.textInput);
-
-    this.httpService.dataFun(this.textInput).subscribe(
-      (result:any) => {        
-          if (result.status === 200) {
+  onClick() {
+    this.httpService.dataFun(this.textInput).subscribe((result: any) => {
+      if (result.status === 200) {
+        this.formData.append("userId", result.data._id);
+        this.httpService.uploadImage(this.formData).subscribe((res: any) => {
+          if (res.status === 200) {
             this.toastr.success(result.message);
-            console.log(result);
-          } 
-          else {
-            this.toastr.error(
-              "Upload unsuccessful."
-            );
+          } else {
+            this.toastr.error("Upload unsuccessful.");
           }
+        });
+      } else {
+        this.toastr.error(result.message);
       }
-    )
+    });
   }
-  displayedColumns: string[] = ['name', 'image',];
-  dataSource = ELEMENT_DATA;
+
+  formData: FormData | null = new FormData();
+  uploadSpecialization(fileToUpload: File) {
+    fileToUpload = fileToUpload[0];
+    this.formData.append("profileImage", fileToUpload, fileToUpload.name);
+    this.formData.append("user", "specializations");
+  }
+  displayedColumns: string[] = ["name", "image"];
+  dataSource = this.ELEMENT_DATA;
 }
