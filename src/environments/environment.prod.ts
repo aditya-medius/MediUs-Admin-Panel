90ff21221263dc59a70561739b25ac6da131618a
aditya-medius <arawat@medius.in>
2024-06-28 00:06:25 +0530

    test build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: "http://3.21.52.154:3000",
-  apiUrl: "http://18.220.60.149:3000",
+  apiUrl: "http://15.207.210.44:3000",
 
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // apiUrl: "http://3.21.52.154:3000",
-    apiUrl: "http://13.126.162.247:3000",
+    apiUrl: "http://15.207.210.44:3000",
   
   };
   
